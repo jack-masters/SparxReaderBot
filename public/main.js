4529dc7cf401c608ed914c735d91f469cb188ec1
Techwithjack123 <105387397+Techwithjack123@users.noreply.github.com>
2025-03-23 16:20:58 +0000

    public/main:

--- public/main.js
+++ public/main.js
@@ -1,11 +1,16 @@
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createServer } from 'node:http';
+
+const port = 2896
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const nodehttpserver = createServer(app)
 
-app.use(cookieParser(cookieSecret));
 app.use('/public', express.static('./public/'))
 
 app.use(express.json());
@@ -14,3 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/html/index.html'));
 })
+
+
+nodehttpserver.listen(port, () => {
+    console.log(`Main Server is running on port: ${port}`);
+});
