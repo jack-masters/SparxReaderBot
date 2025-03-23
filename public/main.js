8cded29980eb451742bb6965e1e148de6fa3875e
Techwithjack123 <105387397+Techwithjack123@users.noreply.github.com>
2025-03-23 16:18:52 +0000

    Main

--- public/main.js
+++ public/main.js
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+const nodehttpserver = createServer(app)
+
+app.use(cookieParser(cookieSecret));
+app.use('/public', express.static('./public/'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    
+})
